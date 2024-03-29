e8d46f5dbebe165e629457d4e1c4ea50ffd76cd8
mkh <mkh@mk.mk>
2024-03-29 14:16:46 +0500

    indexes in migrations created

--- database/migrations/2024_03_17_060758_create_hotels_table.php
+++ database/migrations/2024_03_17_060758_create_hotels_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
             $table->text('description');
             $table->string('address');
             $table->string('phone');
