115106afa335f78e7f7c846cb36f5b58e90a9037
Nodirabegim Musaeva <nojeong13@gmail.com>
2024-04-05 16:04:26 +0500

    Modified booking notification blade

--- resources/views/notification/booking.blade.php
+++ resources/views/notification/booking.blade.php
@@ -9,12 +9,8 @@
 <body>
 <div style="background-color: #f4f4f4; padding: 20px;">
     <h2 style="color: #333;">Booking Confirmation</h2>
-{{--    <p style="color: #333;">Dear {{ $user->name }},</p>--}}
-{{--    <p style="color: #333;">Your booking has been confirmed. Below are the details:</p>--}}
-{{--    <ul>--}}
-{{--        <li><strong>Booking ID:</strong> {{ $booking->id }}</li>--}}
-{{--        <li><strong>Booking Date:</strong> {{ $booking->created_at->format('Y-m-d H:i:s') }}</li>--}}
-{{--    </ul>--}}
+    <h3>Congratulations on behalf of Alif Booking team, you have successfully booked a hotel room. </h3>
+
     <p style="color: #333;">Thank you for using our booking app!</p>
 </div>
 </body>
